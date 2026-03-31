7757d309ea1803a09f7d36353b496e7aa5659d98
Eng. Elaa HAMDANI <elaahamdeny@gmail.com>
2026-03-31 18:34:29 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,14 +1,17 @@
 import tensorflow as tf
-import numpy as np
-from PIL import Image, ImageTk
-import tkinter as tk
-from tkinter import filedialog
+import numpy as np # Manipuler les tableaux de nombres (pixels)
+from PIL import Image, ImageTk # Ouvrir et afficher des images
+import tkinter as tk # Créer la fenêtre et l'interface graphique
+from tkinter import filedialog # Ouvrir l'explorateur de fichiers
 
 # ══════════════════════════════
 # CHARGER L'IA
 # ══════════════════════════════
+
+# On charge le modèle de deep learning déjà entraîné (MobileNetV2) depuis le fichier.
 model = tf.keras.models.load_model("model/solar_defect_model.keras")
 
+# classes : dictionnaire qui associe à chaque numéro de classe un nom de défaut,
 CLASSES = {
     0: ("Bird Drop",    "Moyen",    "#f39c12"),
     1: ("Snow Covered", "Faible",   "#2980b9"),
@@ -18,20 +21,23 @@ CLASSES = {
     5: ("Normal",       "Aucun",    "#27ae60"),
 }
 
+
 # ══════════════════════════════
-# FENÊTRE
+# FENÊTRE 
 # ══════════════════════════════
-root = tk.Tk()
+
+root = tk.Tk() #tk.Tk() crée la fenêtre principale de l'application.
 root.title("Solar Panel Inspector — AI Detection")
 root.configure(bg="white")
 root.configure(bg="#fafafa")
+
 # Taille de l'écran
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 
 # Fenêtre à 80% de l'écran (même ratio)
 width = int(screen_width * 0.8)
-height = int(screen_height * 0.8)
+height = int(screen_height * 0.9)
 
 # Centrer la fenêtre
 x = (screen_width - width) // 2
@@ -40,34 +46,36 @@ y = (screen_height - height) // 2
 root.geometry(f"{width}x{height}+{x}+{y}")
 
 # ══════════════════════════════
-# HEADER
+# HEADER (en-tête)
 # ══════════════════════════════
-header = tk.Frame(root, bg="#C0107C", pady=15)
-header.pack(fill="x")
+header = tk.Frame(root, bg="#02385A", pady=15)
+header.pack(fill="x")# s'étirer sur toute la largeur de la fenêtre.
 
-tk.Label(header,
+tk.Label(header,# Crée un texte dans l'en-tête
          text="☀  Solar Panel Inspector",
          font=("Arial", 20, "bold"),
-         bg="#C0107C", fg="white").pack()
+         bg="#02385A", fg="white").pack()
 
 tk.Label(header,
          text="Inspection intelligente par Drone & Intelligence Artificielle",
          font=("Arial", 11),
-         bg="#C0107C", fg="#9999bb").pack(pady=(3, 0))
+         bg="#02385A", fg="white").pack(pady=(3, 0))
 
 # ══════════════════════════════
 # BOUTON
 # ══════════════════════════════
 btn_frame = tk.Frame(root, bg="white", pady=5)
-btn_frame.pack(fill="x")
+btn_frame.pack(fill="x") 
 
 # ══════════════════════════════
 # CONTENU PRINCIPAL
 # ══════════════════════════════
-main = tk.Frame(root, bg="white")
-main.pack(fill="x", expand=False, padx=40, pady=(10, 0), anchor="n")
-spacer = tk.Frame(root, bg="white")
-spacer.pack(fill="both", expand=True)
+main = tk.Frame(root, bg="white")  # Crée un cadre principal blanc
+main.pack(fill="x", expand=False, padx=40, pady=(10, 0), anchor="n")  # Place le cadre avec marges et alignement haut
+
+spacer = tk.Frame(root, bg="white")  # Crée un cadre vide pour pousser le contenu vers le haut
+spacer.pack(fill="both", expand=True)  # Place le cadre vide pour occuper l'espace restant
+
 # COLONNE GAUCHE — image
 left = tk.Frame(main, bg="white")
 left.pack(side="left", fill="both", expand=True)
@@ -76,14 +84,20 @@ tk.Label(left,
          text="Image thermique",
          font=("Arial", 13, "bold"),
          bg="white", fg="#1a1a2e").pack(anchor="w", pady=(0, 8))
-
+#Le Canvas est une zone de dessin où tu peux afficher des images, dessiner des formes, écrire du texte, et créer des éléments graphiques
 canvas_image = tk.Canvas(left,
-                          width=620, height=560,
+                          width=500, height=400,
                           bg="#f0f0f0",
                           highlightthickness=1,
                           highlightbackground="#dddddd")
 canvas_image.pack(anchor="w", pady=(0, 20))
-canvas_image.create_text(310, 280,
+
+# Calculer le centre du canvas
+centre_x = 500 // 2  # 250 pixels
+centre_y = 400 // 2  # 200 pixels
+
+# Créer le texte centré
+canvas_image.create_text(centre_x, centre_y,
                           text="[ Aucune image chargée ]",
                           font=("Arial", 13),
                           fill="#999999")
@@ -116,34 +130,37 @@ tk.Label(right,
 
 canvas_bar = tk.Canvas(right, width=500, height=30,
                         bg="white", highlightthickness=0)
-canvas_bar.pack(anchor="w", pady=(8, 20))
+canvas_bar.pack(anchor="w", pady=(8, 10))
 
 tk.Frame(right, bg="#eeeeee", height=1).pack(fill="x", pady=8)
 
 tk.Label(right,
          text="Scores par classe :",
          font=("Arial", 12, "bold"),
-         bg="white", fg="#1a1a2e").pack(anchor="w", pady=(10, 8))
+         bg="white", fg="#1a1a2e").pack(anchor="w", pady=(5, 5))
+
 
 labels_scores = {}
 for i, (nom, danger, couleur) in CLASSES.items():
     lbl = tk.Label(right,
                    text=f"{nom}: —",
-                   font=("Arial", 13),
+                   font=("Arial", 11),
                    bg="white", fg="#cccccc")
-    lbl.pack(anchor="w", pady=3)
+    lbl.pack(anchor="w", pady=2)
     labels_scores[i] = lbl
+
 
 # ══════════════════════════════
 # FOOTER
 # ══════════════════════════════
-footer = tk.Frame(root, bg="#f5f5f5", pady=5)
+footer = tk.Frame(root, bg="#f5f5f5", pady=8)
 footer.pack(fill="x", side="bottom")
 
 tk.Label(footer,
-         text="Projet PFA — Inspection photovoltaïque par Drone & IA  |  MobileNetV2 — Deep Learning",
-         font=("Arial", 11),
-         bg="#f5f5f5", fg="#555555").pack(pady=(0, 5))
+         text="Projet PFA 25/26 — Inspection photovoltaïque par Drone & IA ",
+         font=("Arial", 8),
+         bg="#f5f5f5", fg="#555555").pack()
+
 
 # ══════════════════════════════
 # FONCTION ANALYSER — après tout
